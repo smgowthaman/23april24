e19961520fa486817a3ee7f9edd2fe4707eef5c2
“smgowthaman” <smgowthaman@gmail.com>
2024-04-23 15:04:02 +0000

    roll-back version

--- watsapp.java
+++ watsapp.java
@@ -7,7 +7,7 @@ public class SpiralPatternExample2
 //function to print the spiral pattern  
 public static void printPattern(int n)  
 {  
-//detrmines the boundary size of the array  ############day 2 code **********************
+//detrmines the boundary size of the array  
 int size = 2 * n - 1;  
 //inner loop  
 for(int i = 1; i <= size; i++)  
@@ -15,7 +15,7 @@ for(int i = 1; i <= size; i++)
 //outer loop      
 for(int j = 1; j <= size; j++)  
 {  
-//calculates and prints the values for pattern  ********************day 3 Code ---------------------------
+//calculates and prints the values for pattern  
 System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");  
 }  
 System.out.println();  
